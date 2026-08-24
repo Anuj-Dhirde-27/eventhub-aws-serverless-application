5aa531537e48f555c3e643b7e738685212b63d28
Anuj Dhirde <anujdhirde@gmail.com>
2026-08-24 11:29:10 +0530

    Update subscription-lambda.py

--- lambda/subscription-lambda.py
+++ lambda/subscription-lambda.py
@@ -1,1 +1,42 @@
+import json
+import boto3
+import os
 
+sns = boto3.client("sns")
+
+SNS_TOPIC_ARN = os.environ["SNS_TOPIC_ARN"]
+
+
+def lambda_handler(event, context):
+
+    body = json.loads(event.get("body", "{}"))
+
+    email = body.get("email")
+
+    if not email:
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "message": "Email is required"
+            })
+        }
+
+    response = sns.subscribe(
+        TopicArn=SNS_TOPIC_ARN,
+        Protocol="email",
+        Endpoint=email
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Subscription request sent",
+            "subscriptionArn": response["SubscriptionArn"]
+        })
+    }
